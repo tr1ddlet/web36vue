2ddedcd3c4da0722622cd64da5e9f6be5bd73377
tr1ddlet <dwaekkistan@gmail.com>
2026-04-03 20:24:13 +0400

    tr3

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,18 +1,17 @@
-import { createWebHistory, createRouter } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import HomePage from '#root/pages/HomePage.vue'
 import MemoryGamePage from '#root/pages/MemoryGamePage.vue'
 import AboutTurtles from '#root/pages/AboutTurtles.vue'
- 
 
 const routes = [
-  { path: '/web36vue/', component: HomePage },
-  { path: '/web36vue/about', component: MemoryGamePage },
-  { path: '/web36vue/turtles', component: AboutTurtles },
+  { path: '/', component: HomePage },
+  { path: '/about', component: MemoryGamePage },
+  { path: '/turtles', component: AboutTurtles },
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory('/web36vue/'),
   routes,
 })
 
