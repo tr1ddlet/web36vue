6ffd73c98039c9e35aba46a47fb8d0c552d99a87
tr1ddlet <dwaekkistan@gmail.com>
2026-04-03 20:29:25 +0400

    t4

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,23 +1,20 @@
 <template>
-  <p>X:{{ x}}   Y:{{ y }}</p>
+  <p>X: {{ x }}   Y: {{ y }}</p>
+
   <HeaderComponent>
     <p>𓆝</p>
     <p>тэг</p>
   </HeaderComponent>
+
   <div id="conditionalRendering">
     <div v-if="random > 0.5">Сейчас меня видно</div>
     <div v-else>А уже нет</div>
   </div>
 
-  <!-- <ol>
-    <template v-for="todo in todos">
-      <TodoElement :todo="todo"></TodoElement>
-    </template>
-  </ol> -->
   <div class="gallery flex aic jcc">
     <button @click="clickLeft">👈</button>
 
-    <template v-for="image in images">
+    <template v-for="image in images" :key="image.id">
       <img :src="image.src" :alt="image.alt" :style="'display:' + image.isVisible" />
     </template>
 
@@ -27,25 +24,30 @@
   <CalendarTable></CalendarTable>
   <FontManager></FontManager>
 
-  <h1 @click="counterStore.increment">{{counterStore.count}}</h1>
+  <h1 @click="counterStore.increment">{{ counterStore.count }}</h1>
+
+  <div class="nav-buttons">
+    <button @click="goCreator">Go to Creator</button>
+    <button @click="goPethouse">Go to Pethouse</button>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
 import HeaderComponent from "../components/HeaderComponent.vue";
 import CalendarTable from "../components/CalendarTable.vue";
-//import ToDoElement from '../components/TodoElement.vue';
-import { useCounterStore } from '../source/counterCompos.ts';
 import FontManager from "../components/FontManager.vue";
-// import MemoryGame from "../components/MemoryGame.vue";
-// import ForecastTable from "./components/ForecastTable.vue";
-import {useMouse} from '../composables/mouse.ts'
-import { ref } from "vue";
+import { useCounterStore } from '../source/counterCompos.ts';
+import { useMouse } from '../composables/mouse.ts';
+import { useRouter } from 'vue-router';
 
-const {x,y} = useMouse()
-const counterStore = useCounterStore()
-
+const { x, y } = useMouse();
+const counterStore = useCounterStore();
 const random = ref(Math.random());
 
+const router = useRouter();
+
+// Slider
 let images = ref([
   { id: 0, src: "/web36vue/durov.webp", alt: "durov", isVisible: "block" },
   { id: 1, src: "/web36vue/lomonosov.webp", alt: "lomonosov", isVisible: "none" },
@@ -53,21 +55,24 @@ let images = ref([
 ]);
 
 let currentSlide = 0;
-let imagesSize = images.value.length;
+const imagesSize = images.value.length;
 
 const clickLeft = () => {
   currentSlide = (currentSlide - 1 + imagesSize) % imagesSize;
-  resultOfSlider();
+  updateSlider();
 };
+
 const clickRight = () => {
-  currentSlide = (currentSlide + 1) % imagesSize; // после последней пикчи переключается на первый
-  resultOfSlider();
+  currentSlide = (currentSlide + 1) % imagesSize;
+  updateSlider();
 };
-const resultOfSlider = () => {
-  for (let el of images.value) {
-    el.isVisible = el.id == currentSlide ? "block" : "none";
-  }
+
+const updateSlider = () => {
+  images.value.forEach(el => el.isVisible = el.id === currentSlide ? "block" : "none");
 };
+
+const goCreator = () => router.push('/creator');
+const goPethouse = () => router.push('/pethouse');
 
 const todos = ref([
   { text: "анжуманя делат" },
@@ -77,28 +82,12 @@ const todos = ref([
 </script>
 
 <style scoped>
-.flex {
-  display: flex;
-  gap: 20px;
-}
-.fdc {
-  flex-direction: column;
-}
-.jcc {
-  justify-content: center;
-}
-.aic {
-  align-items: center;
-}
-.gallery {
-  display: flex;
-  justify-content: center;
-}
-.gallery img {
-  height: 350px;
-  width: 350px;
-}
-h1 {
-  user-select: none;
-}
+.flex { display: flex; gap: 20px; }
+.fdc { flex-direction: column; }
+.jcc { justify-content: center; }
+.aic { align-items: center; }
+.gallery { display: flex; justify-content: center; }
+.gallery img { height: 350px; width: 350px; }
+h1 { user-select: none; }
+.nav-buttons { margin-top: 20px; gap: 10px; display: flex; }
 </style>
